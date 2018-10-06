64fc2ffc08fcf3840708ba5e4743edc07b08b05c
Sebastian Bolanos <sebasheston@gmail.com>
2018-10-06 10:35:31 -0700

    Install web3 utils

--- backend/scripts/user.js
+++ backend/scripts/user.js
@@ -1,4 +1,7 @@
+
 const ethUtils = require('ethereumjs-util');
+// This should be use this creates a random ether account with no ether
+//web3.eth.accounts.create();
 const privateKey =  '0xf20fe8e18ce0da9a812d4b63c252781b0f75e3143c148e9d52438a5e385d6745'
 module.exports = {
   privateKey,
